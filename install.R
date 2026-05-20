f58fcd7de618697a0edcddc7fb46671268064e82
Taki <zobayer1615@gmail.com>
2026-05-20 11:58:39 +0000

    Add all prediction notebooks and update requirements

--- install.R
+++
@@ -1,7 +0,0 @@
-install.packages("IRkernel")
-install.packages("tidyverse")
-install.packages("rmarkdown")
-install.packages("httr")
-install.packages("shinydashboard")
-install.packages("leaflet")
-IRkernel::installspec()
